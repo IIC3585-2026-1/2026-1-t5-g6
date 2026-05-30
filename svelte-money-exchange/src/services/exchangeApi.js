669ddcadec02eb7e66b93c7a67785c238e1f4b52
Patricio Cortez <patricio.cortez@uc.cl>
2026-05-30 18:06:42 -0400

    request a api de cambio

---
+++ svelte-money-exchange/src/services/exchangeApi.js
@@ -0,0 +1,46 @@
+const API_BASE_URL = 'https://api.frankfurter.dev/v2';
+
+export async function fetchLatestRates(currency = 'CLP') {
+  try {
+    const response = await fetch(`${API_BASE_URL}/rates?base=${currency}`);
+
+    if (!response.ok) {
+      let apiErrorMessage = '';
+      try {
+        const errorData = await response.json();
+        if (errorData && errorData.message) {
+          apiErrorMessage = ` - Detalle de la API: ${errorData.message}`
+        }
+      } catch (parseError) {}
+      
+      throw new Error(`Error al conectar con la API: ${response.status} ${response.statusText}${apiErrorMessage}`);
+    }
+
+    const data = await response.json();
+
+    if (!Array.isArray(data) || data.length === 0) {
+      throw new Error('Respuesta de API vacía o en formato inesperado');
+    }
+
+    const baseCurrency = data[0].base;
+    const lastUpdate = data[0].date;
+
+    const ratesDict = data.reduce((acc, currentItem) => {
+      acc[currentItem.quote] = currentItem.rate;
+      return acc;
+    }, {});
+
+    ratesDict[baseCurrency] = 1;
+
+    return {
+      base: baseCurrency,
+      data: lastUpdate,
+      rates: ratesDict,
+    };
+
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : error;
+    console.error('Fallo en la comunicación con la API:', errorMessage);
+    throw error;
+  }
+}
